38d1b80b9fff1bd8cf6e90268a49b34114f85336
Yanjin Li (YJ) <yl3394@columbia.edu>
2016-11-18 19:14:57 -0500

    Update 002_RandomForest_models_training.R

--- lib/002_RandomForest_models_training.R
+++ lib/002_RandomForest_models_training.R
@@ -81,6 +81,7 @@ getRankbyRF <- function(data){
 # ---------------------------------------------------------------------------------
 
 
+
 # Random Forest Model Training ##########################################################################################
 # Get all Random Forest models (4825) 
 RFmodel4825.list <- getRankbyRF(dat.lyrics.none0)
@@ -105,17 +106,9 @@ dat.features.test.small <- cbind(song_code = dat.features.test$song_code,
                             as.data.frame(dat.features.test.pca$x[,1:12]))
 
 
-
-
-
-
-
-# Transform the probability table into a ranking table 
-
-
 # Random Forest Prediction ##############################################################################################
 # Helper Function 002: Get RF predictions -----------------------------------------
-getPredRankbyRF <- function(model.list, test.data, pred.result) {
+getPredProbbyRF <- function(model.list, test.data, pred.result) {
   # @parameter: a RF models list, a test dataset, an empty prediction table 
   # @value: the filled-out prediction table 
   for (i in (1: length(model.list))){
@@ -125,23 +118,37 @@ getPredRankbyRF <- function(model.list, test.data, pred.result) {
   }
   return(pred.result)
 }
-
+# ---------------------------------------------------------------------------------
 
 # Make an empty dataframe for probability values 
-dat.empty <- dat.lyrics.none0[1:100,] %>% select(c(2:ncol(dat.lyrics.none0)))
-dat.empty <- ifelse(dat.empty<0, NA, NA)
-
-dat.pred.empty <- as.data.frame(cbind(song_code = dat.features.test$song_code, 
-                                     dat.empty))
+dat.empty <- as.data.frame(cbind(song_code = dat.features.test$song_code, 
+                                 ifelse(dat.lyrics.none0[1:100,] %>% 
+                                          select(c(2:ncol(dat.lyrics.none0))) <0, NA, NA)))
+# Make a copy of the empty dataframe 
+dat.pred.prob <- dat.empty 
 
 # Get the prediction results 
-dat.pred.prob <- getPredRankbyRF(RFmodel4825.list, dat.features.test.small, dat.pred.empty)
+dat.pred.prob <- getPredProbbyRF(RFmodel4825.list, dat.features.test.small, dat.pred.empty)
 # Save as a csv file to local
 write.csv(dat.pred.prob, "exported_data/dat_pred_prob.csv")
 
 
+# Ranks Generating ########################################################################################################
+# Helper Function 003: Get RF ranking ---------------------------------------------
+getPredRankbyRF <- function(pred.result) {
+  rank.temp <- t(as.data.frame(rank(-pred.result[1,-1], 
+                                      ties.method = "first")))
+  for(i in (2: nrow(pred.result))){
+    rank.temp <- rbind(rank.temp, 
+                         t(as.data.frame(rank(-pred.result[i,-1], 
+                                              ties.method = "first"))))
+  }
+  rank.result <- data.frame(pred.result[,1], rank.temp)
+  colnames(rank.result) <- colnames(pred.result)
+  return(rank.result)
+}
+# ---------------------------------------------------------------------------------
 
-
-# Random Forest Prediction ##############################################################################################
-
+# Transform the probability table into a ranking table 
+dat.pred.rank <- getPredRankbyRF(dat.pred.prob)
 
